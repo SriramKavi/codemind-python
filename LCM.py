14022af5b9d3dfa8b2353d2957fa98c1454f0ba6
SriramKavi <21A91A04L1@aec.edu.in>
2022-10-05 12:46:11 +0530

    def lcm(a,b) :     ma=list()     mb=list()     k=list()     for i in range(1,50):         ma.append(a*i)     for j in range(1,50):         mb.append(b*j)     for n in ma :         for m in mb :             if n==m :                 k.append(n)     print(min(k))
    a,b=map(int,input().split())
    lcm(a,b)

--- LCM.py
+++ LCM.py
@@ -1,15 +1,18 @@
-def LCM(a,b) :
-    if a>b :
-        maxi=a
-    else :
-        maxi=b
-    v=maxi
-    while True :
-        if maxi%a==0 and maxi%b==0 :
-            print(maxi)
-            break
-        else :
-            maxi=maxi+v
+def lcm(a,b) :
+    ma=list()
+    mb=list()
+    k=list()
+    for i in range(1,50):
+        ma.append(a*i)
+    for j in range(1,50):
+        mb.append(b*j)
+    for n in ma :
+        for m in mb :
+            if n==m :
+                k.append(n)
+    print(min(k))
+                
+
 
 a,b=map(int,input().split())
-LCM(a,b)
+lcm(a,b)
